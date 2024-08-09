ec1da498fe7018c524d00338692b6d88a96ff497
Dawnadrian <101129739+Dawnadrian@users.noreply.github.com>
2024-08-09 17:18:26 +0300

    converted all json refrences to deno kv

--- TaskManager.ts
+++ TaskManager.ts
@@ -1,79 +1,104 @@
 import { Task } from "./task.ts";
 export class TaskManager {
 
-    private tasks: Task[] = [];
-    private filePath: string = "./tasks.json";
 
-    constructor(){
-        this.loadTasks();
-    }
+   private tasks: Task[] = [];
+   private kv: Deno.Kv;
 
-    // loads the json file of the user's tasks every time the program is running
-    private async loadTasks(): Promise<void> {
-        try {
-            const taskData = await Deno.readTextFile(this.filePath);
-            this.tasks = JSON.parse(taskData) as Task[];
-        } catch(error) {
-            if(error instanceof Deno.errors.NotFound) {
-                console.log("no tasks file found, starting an empty list of tasks");
-                this.tasks = []
-            } else {
-                console.error("error loading tasks: ", error);
-            }
-        }
-    }
 
-   
-    
-     // adding a new task from the user
-      addTask( title: string, descritption: string): void {
-        console.log("current tasks: ", this.tasks)
-        const newId = this.tasks.length + 1;
-        const newTask = new Task(newId,title,descritption);
-        this.tasks.push(newTask);
-        console.log(`new task added: ${title}`);
-        this.saveTasks();
+
+
+    constructor() {
+       this.intialize();
     }
 
 
-    // saving the task in the json file
-    private async saveTasks(): Promise<void> {
-        try {
-            const data = JSON.stringify(this.tasks, null, 2);
-            await Deno.writeTextFile(this.filePath,data);
-        } catch(error) {
-            console.error("error saving tsks :" , error);
-        }
-        console.log("new task saved");
-     }
+    private async intialize() {
+       this.kv = await Deno.openKv();
+        await this.loadTasks();
+    }
 
 
-     // prints the tasks and their status
+  
+   // checks if there is a current tasks file in kv store. if so- will load new tasks, else- will create an empty array.
+   private async loadTasks(): Promise<void> {
+       try {
+          const res = await this.kv.get(["tasks"]);
+          if(res.value){
+           this.tasks = res.value as Task[];
+          } else{
+           console.log("no tasks were found in kv store, starting an empty list");
+           this.tasks = [];
+          }
 
-     printTasks(): void {
-        this.tasks.forEach(task => {
-            console.log(`task ID:${task.id} - ${task.title} - ${task.description} [${task.completed ? "completed" : "pending"}]`);
-        });
 
-     }
-    
+       } catch(error){
+           console.error("error loading tasks from kv store: ", error);
+           this.tasks = [];
+       }
+   }
 
-     // deletes the tasks:
-      async deleteTask(idToBeDeleted: number): Promise<void> {
-        try {
-        const taskToBeDeletedData = await Deno.readTextFile(this.filePath);
-        const taskArray = JSON.parse(taskToBeDeletedData) as Task[];
 
-        const updatedTasksList = taskArray.filter(task => task.id !== idToBeDeleted);
-        const updatedData = JSON.stringify(updatedTasksList, null ,2);
-        await Deno.writeTextFile(this.filePath, updatedData);
+ 
+  
+    // adding a new task from the user's tasks array
+     addTask( title: string, description: string): void {
+       console.log("current tasks: ", this.tasks)
+       const newId = this.tasks.length + 1; // assigning a new id number
+       const newTask = new Task(newId,title,description); // creating a new task object
+       this.tasks.push(newTask); // pushing it to tasks array
+       console.log(`new task added: ${title}`);
+       this.saveTasks(this.tasks); // saving the updated array
+   }
 
-        console.log(`task with id ${idToBeDeleted} deleted`);
-        } catch(error) {
-            console.error("error deleting task : ", error);
-        }
-        
 
-     }
 
- }
+
+   // saving the task in the kv store
+   private async saveTasks(taskslist: Task[]): Promise<void> {
+       try {
+           await this.kv.set(["tasks"], taskslist); //setting a new task in the kv store
+
+
+       } catch(error) {
+           console.error("error saving tsks :" , error);
+       }
+       console.log("new task saved");
+    }
+
+
+
+
+    // prints the tasks and their status
+
+
+    printTasks(): void {
+       this.tasks.forEach(task => {
+           console.log(`task ID:${task.id} - ${task.title} - ${task.description} [${task.completed ? "completed" : "pending"}]`);
+       });
+
+
+    }
+  
+
+
+    // deletes a task from the deno kv store by specific id of task:
+     async deleteTask(idToBeDeleted: number): Promise<void> {
+       try {
+           const tempValues = await this.kv.get(["tasks"]);
+           const currentTaskList = tempValues.value as Task[] || []; // if temp.values is undefined it will be cast to an empty array
+           const updatedTaskList = currentTaskList.filter(task => task.id !== idToBeDeleted); //filtering the array by id
+           await this.saveTasks(updatedTaskList); // using ths saveTasks() method to save the new array
+
+
+       } catch(error) {
+           console.error("error deleting task : ", error);
+       }
+      
+
+
+    }
+
+
+}
+
